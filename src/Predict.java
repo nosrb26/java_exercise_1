67817e805c50fb89f3c7f079c5690130041b8613
Pierre Loizeau <pierre.loizeau@epita.fr>
2022-02-10 18:58:09 +0100

    partie 7 begin

--- src/Predict.java
+++ src/Predict.java
@@ -6,123 +6,102 @@ public class Predict implements Command{
         return "predict";
     }
 
-    int max(int[] count) {
+    int maxindex(int[] count) {
         if (count.length > 0) {
-            int i_max = 0;
+            int res = 0;
             int max = count[0];
             for (int i = 0; i < count.length; i++) {
                 if (count[i] > max) {
-                    i_max = i;
+                    res = i;
                     max = count[i];
                 }
             }
-            return i_max;
+            return res;
         }
         return -1;
     }
 
     public boolean run(String entry) {
-        // Create a new scanner
         java.util.Scanner scan = new java.util.Scanner(System.in);
-
-        // Ask for a path
-        System.out.println("Please enter the path to the file:");
+        System.out.println("Veuillez entrer le chemin du fichier :");
         String path_str = scan.nextLine();
-
-        // Get the path
         java.nio.file.Path path = java.nio.file.Paths.get(path_str);
 
         try {
-            // Read the file
             String file = java.nio.file.Files.readString(path);
-
-            // Replace all non alpha by space
             file = file.replaceAll("[^a-zA-Z]", " ");
-
-            // All uppercase to lowercase
             file = file.toLowerCase();
-
-            // Get all the words
             String[] base = file.split(" +");
 
-            // Transform [] into Array
-            java.util.ArrayList<String> no_empty = new java.util.ArrayList<String>();
-            for (String word : base)
-                no_empty.add(word);
-
-            // Sort the list
-            java.util.Collections.sort(no_empty);
-
-            // Create a list with all the word but only one time
-            java.util.ArrayList<String> single = new java.util.ArrayList<String>();
-            for (String word : no_empty){
+            java.util.ArrayList<String> woblk = new java.util.ArrayList<String>();
+            for (String word : base) {
+                woblk.add(word);
+            }
+            java.util.ArrayList<String> uniqword = new java.util.ArrayList<String>();
+            for (String word : woblk){
                 boolean in = false;
-                for (String subword : single){
+                for (String subword : uniqword){
                     if (word.equals(subword)){
                         in = true;
                         break;
                     }
                 }
                 if (!in){
-                    single.add(word);
+                    uniqword.add(word);
                 }
             }
 
-            // get the list of plausible word
-            java.util.ArrayList<String> plausible = new ArrayList<>();
-            for (String word : single){
-                int[] count = new int[single.size()];
+            java.util.ArrayList<String> getable = new ArrayList<>();
+            for (String word : uniqword){
+                int[] count = new int[uniqword.size()];
                 for (int i = 0; i < base.length - 1; i++) {
                     if (word.equals(base[i])) {
                         String next = base[i + 1];
-                        for (int j = 0 ; j < single.size(); j++){
-                            if (next.equals(single.get(j))) {
+                        for (int j = 0 ; j < uniqword.size(); j++){
+                            if (next.equals(uniqword.get(j))) {
                                 count[j] += 1;
                                 break;
                             }
                         }
                     }
                 }
-                int index = max(count);
-                plausible.add(single.get(index));
+                int index = maxindex(count);
+                getable.add(uniqword.get(index));
             }
 
-            // Ask for a word
-            System.out.print("Please enter a word: ");
-            String word = scan.nextLine();
+            System.out.print("Veuillez entrer un mot :");
+            String str = scan.nextLine();
+            str = str.toLowerCase();
 
-            // Predict
             boolean found = false;
-            for (String w : single) {
-                if (w.equals(word)) {
+            for (String w : uniqword) {
+                if (w.equals(str)) {
                     found = true;
-                    // Find the index
                     int index = 0;
-                    while (!single.get(index).equals(word))
+                    while (!uniqword.get(index).equals(str)) {
                         index++;
-                    // Print the most plausible word
-                    System.out.print(plausible.get(index));
-                    word = plausible.get(index);
-                    for (int i = 0; i < 19; i++) {
-
-                        // Find the index
-                        index = 0;
-                        while (!single.get(index).equals(word))
-                            index++;
-
-                        // Print the most plausible word
-                        System.out.print(" " + plausible.get(index));
-                        word = plausible.get(index);
                     }
+
+                    System.out.print(getable.get(index));
+                    str = getable.get(index);
+                    for (int i = 0; i < 19; i++) {
+                        index = 0;
+                        while (!uniqword.get(index).equals(str)) {
+                            index++;
+                        }
+                        System.out.print(" " + getable.get(index));
+                        str = getable.get(index);
+                    }
+                    System.out.println();
                     break;
                 }
             }
-            // if the word isn't in the text
-            if (!found)
-                System.out.println("The world you provided isn't in the text you provided");
+
+            if (!found) {
+                System.out.println("Le mot indiqué n'est pas dans le texte donné");
+            }
         }
 
-        // Catch an error
         catch (java.io.IOException e){
             System.out.println("Unreadable file: " + e);
         }
